1d1b77646a1cd0c1e431c422f23aa907dff16e37
Alina Zhulanava <zhulanavaa@gmail.com>
2024-07-30 02:01:00 +0200

    test user login

--- tests/test_element_arrivals.py
+++ tests/test_element_arrivals.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -65,6 +67,47 @@ def test_search_registration(browser, get_url):
         element = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.XPATH, "//h1[text()='Register Account']"))
     )
+    except NoSuchElementException:
+        assert False
+    assert True
+
+def test_user_login(browser, get_url):
+    browser = browser
+    url = get_url + "/en-gb?route=account/login"
+    browser.get(url)
+
+    try:
+        email = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//input[@name='email']"))
+        )
+        email.send_keys("ali.zhulanova@gmail.com")
+        password = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//input[@name='password']"))
+        )
+        password.send_keys("password")
+        button = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[text()='Login']"))
+        )
+        button.click()
+    except NoSuchElementException:
+        assert False
+
+    try:
+        WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//h2[text()='My Account']"))
+        )
+        dropdown = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//i[@class='fa-solid fa-user']"))
+        )
+        dropdown.click()
+    except NoSuchElementException:
+        assert False
+
+    try:
+        logout = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//a[text()='Logout']"))
+        )
+        logout.click()
     except NoSuchElementException:
         assert False
     assert True
