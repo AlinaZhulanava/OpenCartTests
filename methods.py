3ba6316245243fade4d4f685e509f51d6e548d17
Alina Zhulanava <zhulanavaa@gmail.com>
2024-07-31 00:55:49 +0200

    +scenario

--- methods.py
+++ methods.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -60,3 +61,19 @@ def wait_elem_clickable_by_xpath(browser, elem_xpath):
 
 def scroll_to_elem(browser, element):
     browser.execute_script("arguments[0].scrollIntoView(true);", element)
+
+def get_prices_list(all_prices_elems):
+    prices_list = []
+    for price_elem in all_prices_elems:
+        prices_list.append(price_elem.text)
+    return prices_list
+
+
+def change_currency(browser):
+    currency_choose_button = get_elem_by_xpath(browser, "//span[text()='Currency']")
+    time.sleep(10)
+    currency_choose_button.click()
+    currency_button = get_elem_by_xpath(browser, "//a[@href='EUR']")
+    time.sleep(10)
+    currency_button.click()
+
